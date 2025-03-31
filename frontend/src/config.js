5faf885dde2e4397ab858b7029710111b2339a28
Ahmad Baker <ahmadqadoomy79@gmail.com>
2025-03-30 22:25:23 -0600

    updated config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const IS_PROD = import.meta.env.VITE_API_MODE === "production";
-
+// deploy
 export const API_BASE_URL = IS_PROD
   ? "https://cybersim-backend.onrender.com"
   : "http://localhost:5000";
